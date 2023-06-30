34f918056455245525fa505ab41e5628ecfd5e4d
Jimscwang <96602156+Jimscwang@users.noreply.github.com>
2023-06-30 14:53:58 +0800

    Add files via upload

---
+++ card4/js/all.js
@@ -0,0 +1,35 @@
+(()=>{
+    const exampleModal = document.getElementById('removeModal')
+    if (exampleModal) {
+    exampleModal.addEventListener('show.bs.modal', event => {
+        // Button that triggered the modal
+        const button = event.relatedTarget
+        // Extract info from data-bs-* attributes
+        const recipient = button.getAttribute('data-title')
+        // If necessary, you could initiate an Ajax request here
+        // and then do the updating in a callback.
+
+        // Update the modal's content.
+        const modalTitle = exampleModal.querySelector('.modal-title')
+        modalTitle.textContent = `確認 ${recipient}`
+    })
+    }
+
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+        }, false)
+    })
+
+})()
