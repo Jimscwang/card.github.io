ff426405c324bfadda187f60787e56f78ef65bad
Jimscwang <96602156+Jimscwang@users.noreply.github.com>
2023-07-14 17:13:48 +0800

    Delete card2 directory

--- card2/js/header.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-        $('.nav-menu-state').click(function(e){
-        e.preventDefault();
-        $(this).children('.nav-menu-icon').toggleClass('d-none');
-        $(this).children('.clear-icon').toggleClass('d-none');
-        $(this).parents().siblings('.nav-menu').toggleClass('d-none');
-    });
-})
