99c54838c290beb76adf7ebc49a6cf3f3754b980
Jimscwang <96602156+Jimscwang@users.noreply.github.com>
2023-07-17 14:39:04 +0800

    Delete card2 directory

--- card2/js/QA.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('.answer').hide();
-    $('.QA-column').click(function(e){
-        e.preventDefault();
-        $(this).children('.answer').slideToggle(250);
-        $(this).children().children('.add-icon').toggle().siblings('.remove-icon').toggleClass('d-none');
-    });
-})
-
